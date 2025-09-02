9589ed84dd6926add35128f737c4dba4917329f4
junkfood <69683722+JunkFood02@users.noreply.github.com>
2025-09-03 00:24:05 +0800

    chore: bump up version to 0.15.2 (43)

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -38,8 +38,8 @@ android {
         applicationId = "me.ash.reader"
         minSdk = 26
         targetSdk = 34
-        versionCode = 42
-        versionName = "0.15.1"
+        versionCode = 43
+        versionName = "0.15.2"
 
         buildConfigField(
             "String",
